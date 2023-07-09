f34bf3e822d1c66a4b4c25e1f0259d9a22d1bb5a
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-09 13:33:21 +0530

    time to write test

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -4,24 +4,24 @@ pragma solidity ^0.8.19;
 
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-/** 
-* @title DSCEngine
-* @author Pandit Dhamdhere
-*
-* The system is designed to be as minimal as passible, and have the tokens maintain a 1 token == $1 peg.
-* This stablecoin has the properties: 
-* -Exogenous Collateral 
-* -Doller pegged
-* -Algorithmically Stable
-
-* It is similar to DAI if DAI had no governance , no fees, and was only backed by only wETH and wBTC.
-* Our DSC system should always be "overcollateralized". At no point, should the value of all collateral <= the $ backed value of all the DSC .
-*
-* @notice This contract is the core of the DSC System. It handles all the logic for minting and redeeming DSC, as well as depositing and withdrawing collateral.
-* @notice This contract is VERY loosly based on the makerDAO DSS(DAI) system.
-
-*/
+/**
+ * @title DSCEngine
+ * @author Pandit Dhamdhere
+ *
+ * The system is designed to be as minimal as passible, and have the tokens maintain a 1 token == $1 peg.
+ * This stablecoin has the properties:
+ * -Exogenous Collateral
+ * -Doller pegged
+ * -Algorithmically Stable
+ *
+ * It is similar to DAI if DAI had no governance , no fees, and was only backed by only wETH and wBTC.
+ * Our DSC system should always be "overcollateralized". At no point, should the value of all collateral <= the $ backed value of all the DSC .
+ *
+ * @notice This contract is the core of the DSC System. It handles all the logic for minting and redeeming DSC, as well as depositing and withdrawing collateral.
+ * @notice This contract is VERY loosly based on the makerDAO DSS(DAI) system.
+ */
 
 contract DSCEngine is ReentrancyGuard {
     ///////////////
@@ -30,17 +30,31 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__NeedsMoreThanZero();
     error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
     error DSCEngine__NotAllowedToken();
+    error DSCEngine__TransferFailed();
 
-    /////////////// ////////
+    //////////////////////////
     // State variables    //
     //////////////// ///////
 
     mapping(address token => address priceFeeds) private s_priceFeeds; //tokenToPriceFeeds;
+    mapping(address user => mapping(address token => uint256 amount))
+        private s_collateralDeposited;
 
     DecentralizedStableCoin private immutable i_dsc;
+
+    /////////////// ////////
+    //   events           //
+    //////////////// ///////
+    event CollateralDeposited(
+        address indexed user,
+        address indexed token,
+        uint256 indexed amount
+    );
+
     ////////////////
     // modifiers  //
     ////////////////
+
     modifier moreThanZero(uint256 amount) {
         if (amount == 0) {
             revert DSCEngine__NeedsMoreThanZero();
@@ -80,6 +94,7 @@ contract DSCEngine is ReentrancyGuard {
     function depositCollateralAndMintDsc() external {}
 
     /*
+     * @notice follows CEI pattern
      * @param tokenCollateralAddress The Adress of the token to deposit as collateral
      * @param amountCollateral The amount of collateral to deposit
      */
@@ -92,7 +107,24 @@ contract DSCEngine is ReentrancyGuard {
         moreThanZero(amountCollateral)
         isAllowedToken(tokenCollateralAddress)
         nonReentrant
-    {}
+    {
+        s_collateralDeposited[msg.sender][
+            tokenCollateralAddress
+        ] += amountCollateral;
+        emit CollateralDeposited(
+            msg.sender,
+            tokenCollateralAddress,
+            amountCollateral
+        );
+        bool success = IERC20(tokenCollateralAddress).transferFrom(
+            msg.sender,
+            address(this),
+            amountCollateral
+        );
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
+    }
 
     function redeemCollateralForDsc() external {}
 
