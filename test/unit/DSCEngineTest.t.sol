9d4ecf86aa56b2054b11f41224f6854e1d2d8d89
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-11 09:04:55 +0530

    test failed

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+    DecentralizedStableCoin dsc;
+    DSCEngine dsce;
+    HelperConfig config;
+    address ethUsdPriceFeed;
+    address weth;
+
+    function setup() public {
+        deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
+        (ethUsdPriceFeed, , weth, , ) = config.activeNetworkConfig();
+    }
+
+    //////////////// ///
+    /// price Tests ///
+    ///////////////////
+    function testGetUsdValue() public {
+        uint256 ethAmount = 15e18;
+        // 15e18 * 2000/ETH = 30000e18;
+        uint256 expectedUsd = 30000e18;
+        uint256 actualUsd = dsce.getUsdValue(weth, ethAmount);
+        assertEq(expectedUsd, actualUsd);
+    }
+}
